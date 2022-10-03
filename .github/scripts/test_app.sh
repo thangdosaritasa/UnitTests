825510e8863a595e0de3d86fde96fef985ed6e3c
Thang Do <dothang@domacmini.routers.saritasa.io>
2022-10-03 14:51:15 +0700

    - update

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -project UnitTest.xcodeproj \
-            -scheme UnitTest\ iOS \
+            -scheme UnitTest \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
